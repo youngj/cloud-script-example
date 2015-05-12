737f7715ab9169c44feee25052b50237d69f3741
Jesse Young <youngj@telerivet.com>
2015-05-12 10:59:46 -0700

    example config

--- commands/unsubscribe.js
+++ commands/unsubscribe.js
@@ -1,4 +1,6 @@
 
-var subscribers = project.getOrCreateGroup("Subscribers");
+var config = require('../config');
+
+var subscribers = project.getOrCreateGroup(config.GROUP_NAME);
 contact.removeFromGroup(subscribers);
 contact.save();
---
+++ config.js
@@ -0,0 +1,9 @@
+/*
+ * Example module for exporting constants to use in multiple files.
+ * (It is not required to have a config.js file -- this acts just like any other module)
+ */
+
+module.exports = {
+    FOO: 42,
+    GROUP_NAME: "Subscribers"
+};
