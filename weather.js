da9456f0d922bff6f9f9124608f9faad8e313634
Jesse Young <adunar@gmail.com>
2016-11-30 14:46:45 -0800

    Update weather.js

--- weather.js
+++ weather.js
@@ -3,13 +3,23 @@
  * to the `exports` variable.
  */
 
-exports.getWeatherForZIPCode = function(zipcode) { 
-    var url = "http://weather.yahooapis.com/forecastrss";
+exports.getWeather = function(placeName) { 
+    var url = "https://query.yahooapis.com/v1/public/yql";
+    
+    var escapedName = placeName.replace(/\\/g, "\\\\").replace(/'/g, "\\'");
+    
+    var yql = "select * from weather.forecast where woeid in "
+        + "(select woeid from geo.places(1) where text = '" + escapedName + "')";
+    
     var response = httpClient.request(url, {
         method: 'GET',
-        params: {p: zipcode}
+        params: {
+            q: yql,
+            format: 'xml',
+            env: 'store://datatables.org/alltableswithkeys'
+        }
     });
-     
+    
     var parser = new DOMParser();
     var dom = parser.parseFromString(response.content, 'text/xml');
      
@@ -18,9 +28,8 @@ exports.getWeatherForZIPCode = function(zipcode) {
     var conditionElem = dom.getElementsByTagNameNS(WEATHER_API_NS, 'condition')[0];
     var locationElem = dom.getElementsByTagNameNS(WEATHER_API_NS, 'location')[0];
      
-    if (!locationElem)    
-    {
-        return "Sorry, we couldn't find the zipcode " + zipcode + ".";
+    if (!locationElem) {
+        return "Sorry, we couldn't find the weather for " + placeName + ".";
     }
      
     var location = locationElem.getAttribute("city") + 
