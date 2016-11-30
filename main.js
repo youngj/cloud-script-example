575770b8d30eed60f1c952ce631411e7ecbfa4ac
Jesse Young <adunar@gmail.com>
2016-11-30 14:42:27 -0800

    Update example to use main()

--- main.js
+++ main.js
@@ -5,30 +5,34 @@
  * and only require the modules needed to handle the current message.
  */
 
-var command = (word1 || "").toLowerCase();
+global.main = function() {
+    
+    var command = (word1 || "").toLowerCase();
 
-switch (word1)
-{
-    case "hello":
-    case "hi":
-        require("./commands/hello");
-        break;
-    case "join":
-    case "subscribe":
-        require("./commands/subscribe");
-        break;
-    case "stop":
-    case "unsubscribe":
-    case "cancel":
-        require("./commands/unsubscribe");
-        break;
-    case "bitcoin":
-        var coindesk = require('./coindesk');
-        sendReply("1 BTC = $" + coindesk.getCurrentBitcoinPriceUSD());
-        break;
-    case "weather":
-        require("./commands/weather-zip");
-        break;        
-    default:
-        console.log("Unknown keyword: " + word1);
-}
+    switch (word1)
+    {
+        case "hello":
+        case "hi":
+            require("./commands/hello");
+            break;
+        case "join":
+        case "subscribe":
+            require("./commands/subscribe");
+            break;
+        case "stop":
+        case "unsubscribe":
+        case "cancel":
+            require("./commands/unsubscribe");
+            break;
+        case "bitcoin":
+            var coindesk = require('./coindesk');
+            sendReply("1 BTC = $" + coindesk.getCurrentBitcoinPriceUSD());
+            break;
+        case "weather":
+            require("./commands/weather-zip");
+            break;        
+        default:
+            console.log("Unknown keyword: " + word1);
+    }
+    
+};
